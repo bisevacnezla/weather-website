03e960ac8b06982458be7675a82a69cb243c2707
unknown <bisevacnezla@gmail.com>
2020-08-04 17:12:39 +0200

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+var messageOne= document.querySelector('#message-1')
+var messageTwo= document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const loc = search.value
+
+    fetch('http://localhost:3000/weather?address=' + loc).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            messageOne.textContent = ''
+            messageTwo.textContent = data.error
+            console.log(data.error)
+        }
+        else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+
+})
+
